f33c2aa20fd7ec318fef153c92269707c11e5c4e
chys <milathina@gmail.com>
2026-05-01 22:38:11 +0700

    Update planner.dart

--- lib/screens/planner.dart
+++ lib/screens/planner.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'planner_detail.dart';
 import 'package:intl/intl.dart';
-import 'plannerDetail.dart';
 import '../database.dart';
 
 class PlannerScreen extends StatefulWidget {
